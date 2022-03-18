16ab6c88c410319295621012b00b78ad0dbca3e8
Arthur Gonçalves Breguez <98524696+ArtBreguez@users.noreply.github.com>
2022-03-17 21:32:49 -0300

    Update users.sol

--- users.sol
+++ users.sol
@@ -1,81 +1,29 @@
-pragma solidity 0.4.18;
-//SPDX-License-Identifier: MIT
-contract UserSystem {
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
-    struct UserInfo {
+contract users {
+
+    struct Users {
         string Name;
-        uint Age;
         uint CPF;
+        uint Age;
         bool HasVoted;
-        uint Index;
     }
 
-    uint[] private Users;
-   
-    
-    mapping (uint => UserInfo) private UserData;
+    mapping(uint => Users) usuarios;
 
-    event LogNewUser(uint indexed _CPF, uint Index, string Name, uint Age);
-    event LogUpdateUser(uint indexed _CPF, uint Index, string Name, uint Age);
-    event LogDeletedUser(uint indexed _CPF, uint Index);
-    
-    function UserExist(uint _CPF) public constant returns(bool isUser) {
-        if(Users.length == 0) return false;
-        return (Users[UserData[_CPF].Index] == _CPF);
+    function addUser(string calldata _Name, uint _CPF, uint _Age) external{
+        Users memory usuario = Users(_Name, _CPF, _Age, false);
+        usuarios[_CPF] = usuario;
     }
-    function AddNewUser(string _Name, uint _Age, uint _CPF) public returns(uint Index){
-            if(UserExist(_CPF)) throw;
-            UserData[_CPF].Name = _Name;
-            UserData[_CPF].Age = _Age;
-            UserData[_CPF].CPF = _CPF;
-            UserData[_CPF].HasVoted = false;
-            LogNewUser(
-                _CPF,
-                UserData[_CPF].Index,
-                _Name,
-                _Age
-            );
-            UserData[_CPF].Index = Users.push(_CPF) -1;        
-            return Users.length-1;
+    function getUser(uint _CPF) external view returns(string memory Name, uint Age, bool HasVoted) {
+        return(usuarios[_CPF].Name, usuarios[_CPF].Age, usuarios[_CPF].HasVoted);
     }
-
-    function ViewUser(uint _CPF) public constant returns (string Name, uint Age, bool HasVoted) {
-        if(!UserExist(_CPF)) throw;
-        return (UserData[_CPF].Name, UserData[_CPF].Age, UserData[_CPF].HasVoted);
+    function updateUser(string calldata _Name, uint _CPF, uint _Age) external {
+        usuarios[_CPF].Name = _Name;
+        usuarios[_CPF].Age = _Age;
     }
-    function GetUsersCount() public constant returns(uint count) {
-        return Users.length;
-    }
-    function GetUserAtIndex(uint Index) public constant returns(uint CPF) {
-        return Users[Index];
-    }
-    function UserAgeUpdate(uint _CPF, uint _Age) public returns(bool success) {
-        if(!UserExist(_CPF)) throw;
-        UserData[_CPF].Age = _Age;
-        LogUpdateUser(
-                _CPF,
-                UserData[_CPF].Index,
-                UserData[_CPF].Name,
-                _Age
-            );
-        return true;
-    }
-    function DeleteUser(uint _CPF) public returns (uint Index) {
-        if(!UserExist(_CPF)) throw;
-        uint rowToDelete = UserData[_CPF].Index; // armazena em rowToDelete o indice do registro que se deseja deletar
-        uint keyToMove = Users[Users.length-1]; //Pega o ultimo registro da lista e salva em keyToMove
-        Users[rowToDelete] = keyToMove; //Coloca o ultimo registro da lista no lugar do registro que se deseja apagar
-        UserData[keyToMove].Index = rowToDelete; //atualiza o indice do registro que foi movido pro espaço do que foi deletado
-        Users.length--; //Dropa a ultima coluna da lista
-        LogDeletedUser(
-            _CPF,
-            rowToDelete
-        );
-        LogUpdateUser(
-            keyToMove,
-            rowToDelete,
-            UserData[_CPF].Name,
-            UserData[_CPF].Age
-        );
+    function deleteUser(uint _CPF) external {
+        delete usuarios[_CPF];
     }
 }
