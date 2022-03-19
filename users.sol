424638f37b5a97a5ef7f212f58b4c6c43cef3329
Arthur Gonçalves Breguez <98524696+ArtBreguez@users.noreply.github.com>
2022-03-19 19:02:20 -0300

    Incrementando a função de usuarios
    falta pouca coisa pra terminar ela (alguns modifiers e etc)

--- users.sol
+++ users.sol
@@ -1,6 +1,11 @@
 pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
+contract InterfaceUsers {
+    function viewUserStatus(uint _CPF) external view returns(bool HasVoted);
+    function updateUserStatus(uint _CPF) external;
+}
+
 contract users {
 
     struct Users {
@@ -12,18 +17,52 @@ contract users {
 
     mapping(uint => Users) usuarios;
 
+    Users[] CurrentUsers;
+
+    event LogNewUser(uint indexed _CPF, string Name, uint Age);
+    event LogUpdateUser(uint indexed _CPF, string Name, uint Age);
+    event LogDeletedUser(uint indexed _CPF, string Name);
+
     function addUser(string calldata _Name, uint _CPF, uint _Age) external{
+        require(_CPF != 0, "O CPF NÃO PODE SER ZERO!");
+        require(usuarios[_CPF].CPF == 0, "O CPF JA ESTÁ CADASTRADO!");
         Users memory usuario = Users(_Name, _CPF, _Age, false);
         usuarios[_CPF] = usuario;
+        emit LogNewUser(
+            _CPF,
+            _Name,
+            _Age
+        );
     }
     function getUser(uint _CPF) external view returns(string memory Name, uint Age, bool HasVoted) {
+        require(usuarios[_CPF].CPF != 0);
         return(usuarios[_CPF].Name, usuarios[_CPF].Age, usuarios[_CPF].HasVoted);
     }
+    function viewUserStatus(uint _CPF) external returns(bool HasVoted) {
+        require(usuarios[_CPF].CPF != 0, "O USUARIO NÃO EXISTE!");
+        return(usuarios[_CPF].HasVoted);
+    }
     function updateUser(string calldata _Name, uint _CPF, uint _Age) external {
+        require(_CPF != 0, "O CPF NÃO PODE SER ZERO!");
+        require(usuarios[_CPF].CPF != 0, "O USUARIO NÃO EXISTE!");
         usuarios[_CPF].Name = _Name;
         usuarios[_CPF].Age = _Age;
+        emit LogUpdateUser(
+            _CPF,
+            _Name,
+            _Age
+        );
+    }
+    function updateUserStatus(uint _CPF) external {
+        require(usuarios[_CPF].CPF != 0, "O USUARIO NÃO EXISTE!");
+        usuarios[_CPF].HasVoted = true;
     }
     function deleteUser(uint _CPF) external {
+        require(usuarios[_CPF].CPF != 0, "O USUARIO NÃO EXISTE!");
         delete usuarios[_CPF];
+        emit LogDeletedUser(
+            _CPF,
+            usuarios[_CPF].Name
+        );
     }
 }
