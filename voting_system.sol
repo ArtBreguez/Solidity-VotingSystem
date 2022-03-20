a27d5cb6f278124f1571dc42dbadff1172ae519d
Arthur Gonçalves Breguez <98524696+ArtBreguez@users.noreply.github.com>
2022-03-20 18:38:34 -0300

    Update voting_system.sol
    comentarios adicionados

--- voting_system.sol
+++ voting_system.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
+/// @title Blockchain voting system simulator
+/// @author Arthur Gonçalves Breguez
+/// @notice Use this contract with "users.sol", "candidates.sol" and "ownable.sol" to simulate a voting system on blockchain
 import "./users_new.sol";
 import "./new_candidates.sol";
 import "./ownable.sol";
@@ -9,19 +12,34 @@ contract Voting is Ownable {
 
     address candidates_contract;
     address users_contract;
-    
+
+    /// @notice Emit a log when a new vote is made
     event LogNewVote(uint indexed _CPF, bool HasVoted);
 
-    function setAddresToCandidadatesContract(address _address) external onlyOwner{
+    /// @notice function to set the conctract address of "candidates.sol"
+    /// @param _address Candidates contract address
+    /// @return true Confirmation that the address is set
+    function setAddresToCandidadatesContract(address _address) external onlyOwner returns(bool contractSet){
         candidates_contract = _address;
+        return true;
     }
-    function setAddresToUsersContract(address _address) external onlyOwner{
+    /// @notice function to set the conctract address of "users.sol"
+    /// @param _address Users contract address
+    /// @return true Confirmation that the address is set
+    function setAddresToUsersContract(address _address) external onlyOwner returns(bool conctractSet){
         users_contract = _address;
+        return true;
     }
+    /// @notice function View the candidate Name using the candidate Number
+    /// @param _Number Candidate number
+    /// @return Name Candidate name
     function viewCandidate(uint _Number) external view returns (string memory Name) {
         InterfaceCandidates view_candidate = InterfaceCandidates(candidates_contract);
         return view_candidate.getCandidate(_Number);
     }
+    /// @notice Function to perform the vote, one vote for each person
+    /// @param _CPF Unique cpf of the person who votes,
+    /// @param _CandidateNumber Is the candidate Number of the election 
     function vote(uint _CPF, uint _CandidateNumber) external{
         InterfaceCandidates candidate = InterfaceCandidates(candidates_contract);
         InterfaceUsers user = InterfaceUsers(users_contract);
