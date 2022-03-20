fa72a1b80faeca7b8bc5dc24fb8f6317a346ddfb
Arthur Gonçalves Breguez <98524696+ArtBreguez@users.noreply.github.com>
2022-03-20 15:03:16 -0300

    atualização no contrato

--- voting_system.sol
+++ voting_system.sol
@@ -3,18 +3,19 @@ pragma experimental ABIEncoderV2;
 
 import "./users_new.sol";
 import "./new_candidates.sol";
+import "./ownable.sol";
 
-contract Voting {
+contract Voting is Ownable {
 
     address candidates_contract;
     address users_contract;
     
     event LogNewVote(uint indexed _CPF, bool HasVoted);
 
-    function setAddresToCandidadatesContract(address _address) external {
+    function setAddresToCandidadatesContract(address _address) external onlyOwner{
         candidates_contract = _address;
     }
-    function setAddresToUsersContract(address _address) external {
+    function setAddresToUsersContract(address _address) external onlyOwner{
         users_contract = _address;
     }
     function viewCandidate(uint _Number) external view returns (string memory Name) {
